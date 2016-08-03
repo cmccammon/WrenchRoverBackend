b0ab84ab69d226d9f3742b83e8612d52308102f3
Chris McCammon <cmccammon@gmail.com>
2016-08-03 16:05:52 -0700

    adding CORS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:create, :show, :update, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
   def index
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   # DELETE /users/1
   def destroy
     @user.destroy
