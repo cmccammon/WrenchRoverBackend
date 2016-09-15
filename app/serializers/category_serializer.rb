1da527d07dc41b2ff1a36a5afed8a85125a2a2a4
Chris McCammon <cmccammon@gmail.com>
2016-09-15 09:58:54 -0700

    cleaned up model and serializer now that it's working

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,7 +1,7 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name
-  
-  has_many :children, class_name: "Category", foreign_key: :parent_id
-  belongs_to :parent, class_name: "Category", foreign_key: :id, optional: true
+  attributes :id, :name, :parent_id
+
+  # has_many :children, class_name: "Category", foreign_key: :parent_id
+  # belongs_to :parent, class_name: "Category", foreign_key: :id, optional: true
 
 end
