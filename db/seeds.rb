1c09d119e1c084557d8d1a435ecb3630c12a7fb7
Chris McCammon <cmccammon@gmail.com>
2016-09-19 14:14:36 -0700

    working on seed file for categories data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-15.times do
-  user = User.new ({
-    user_name: Faker::Internet.user_name,
-    user_email: Faker::Internet.safe_email,
-    user_zip: Faker::Address.zip,
-    user_phone: Faker::PhoneNumber.phone_number
-    })
-    user.save!
-end
 
-10.times do
-  request = ServiceRequest.new ({
-    request_issue: Faker::ChuckNorris.fact,
-    request_auto_data: Faker::Vehicle.manufacture,
-    user_id: rand(1..14)
-    })
-    request.save!
-end
+                # var = Category.create!(:name => 'Name')
+                # name.move_to_child_of(parent)
 
-15.times do
-  service = ServiceCenter.new ({
-    service_name: Faker::Company.name,
-    service_address: Faker::Address.street_address,
-    service_city: Faker::Address.city,
-    service_state: Faker::Address.state,
-    service_zip: Faker::Address.zip,
-    service_email: Faker::Internet.safe_email,
-    service_phone: Faker::PhoneNumber.phone_number,
-    service_bio: Faker::StarWars.quote
-    })
-    service.save!
-end
+
+
+
+
+
+
+
+
+# 15.times do
+#   user = User.new ({
+#     user_name: Faker::Internet.user_name,
+#     user_email: Faker::Internet.safe_email,
+#     user_zip: Faker::Address.zip,
+#     user_phone: Faker::PhoneNumber.phone_number
+#     })
+#     user.save!
+# end
+#
+# 10.times do
+#   request = ServiceRequest.new ({
+#     request_issue: Faker::ChuckNorris.fact,
+#     request_auto_data: Faker::Vehicle.manufacture,
+#     user_id: rand(1..14)
+#     })
+#     request.save!
+# end
+#
+# 15.times do
+#   service = ServiceCenter.new ({
+#     service_name: Faker::Company.name,
+#     service_address: Faker::Address.street_address,
+#     service_city: Faker::Address.city,
+#     service_state: Faker::Address.state,
+#     service_zip: Faker::Address.zip,
+#     service_email: Faker::Internet.safe_email,
+#     service_phone: Faker::PhoneNumber.phone_number,
+#     service_bio: Faker::StarWars.quote
+#     })
+#     service.save!
+# end
