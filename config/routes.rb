a12a05558a308f0e417165855aad7bd290f09265
Chris McCammon <cmccammon@gmail.com>
2016-11-08 08:36:12 -0800

    pushing to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,10 @@ Rails.application.routes.draw do
   resources :users
 
   get  'service_requests_profile', to: 'service_requests#userid'
-  # get 'profile', to: :show, controller: 'users'
+
   post 'authenticate', to: 'authentication#authenticate'
+
+  
   # get    '/categories/list'     => 'categories#list'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
