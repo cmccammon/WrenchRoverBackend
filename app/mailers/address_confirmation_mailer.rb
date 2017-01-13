55636bb35155416503089e2290c365ba464d64e5
Chris McCammon <cmccammon@gmail.com>
2017-01-12 16:37:02 -0800

    changed label back in category there was a conflict

--- app/mailers/address_confirmation_mailer.rb
+++ app/mailers/address_confirmation_mailer.rb
@@ -2,6 +2,7 @@ class AddressConfirmationMailer < ApplicationMailer
 
   def address_confirmation_email(user)
     @user = user
+    @token = JsonWebToken::encode(user_id: user.id)
     mail(to: @user.user_email, subject: "Welcome to WrenchRover!")
   end
 
